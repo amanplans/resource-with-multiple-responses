6fc697d85a3bc6dceb237971914af952059e3ac3
Andy Man <dev@withaplan.nl>
2025-01-26 17:36:32 +0100

    Cleanup example 5

--- src/example-5.component.ts
+++ src/example-5.component.ts
@@ -24,29 +24,26 @@ import { HttpClient } from "@angular/common/http";
   }`,
 })
 export class Example5Component {
-  httpClient = inject(HttpClient);
+  #httpClient = inject(HttpClient);
 
   constructor() {
     this.loadCats();
+  }
+
+  protected catCounter = signal(0);
+  private loadCatsCounter(): void {
+    this.catCounter.update((counter) => counter + 1);
   }
 
   private cats = signal<{
     value: Cat[];
   }>({ value: [] });
   private loadCats() {
-    console.log("Load cats");
     interval(3000) // every second
       .pipe(take(5)) // repeat 5 times
       .subscribe(() => {
-        console.log("interval");
         this.loadCatsCounter();
-        // async () => {
-        //   console.log('In async of loadCats');
-        // const fetchedCats = await this.fetchRemoteData();
-        // this.cats.set({ value: fetchedCats});
-        // }
         this.fetchRemoteData2().subscribe((cats) => {
-          // this.cats.set({ value: cats});
           this.cats.update((cats2) => { 
             return { value: [...cats2.value, ...cats] }
           });
@@ -54,53 +51,14 @@ export class Example5Component {
       });
   }
 
-  protected catCounter = signal(0);
-  protected loadCatsCounter() {
-    this.catCounter.update((counter) => counter + 1);
-  }
-
-  private async fetchRemoteData() {
-    console.log("Fetching cats from the api");
-    // this.httpClient
-    //   .get<Cat[]>('https://api.thecatapi.com/v1/images/search?limit=10');
-    const cats = await fetch(
-      "https://api.thecatapi.com/v1/images/search?limit=10"
-    ).then((res) => res.json() as Promise<Cat[]>);
-    return cats;
-  }
-
   private fetchRemoteData2(): Observable<Cat[]> {
-    console.log("Fetching cats 2 from the api");
-    const cats$ = this.httpClient.get<Cat[]>(
+    const cats$ = this.#httpClient.get<Cat[]>(
       "https://api.thecatapi.com/v1/images/search?limit=10"
     );
     return cats$;
   }
 
   protected readonly resource = resource<Cat[], Cat[]>({
-    // request: () => ({cats: this.cats()}),
     stream: async () => this.cats,
-    // stream: async (request) => {
-    //   const wrappedResponse = signal<{
-    //     value: Cat[];
-    // } | {
-    //     error: unknown;
-    // }>({ value: []});
-    //   try {
-    //     console.log('In the stream fetching cats');
-    //     const response = await this.fetchRemoteData();
-    //     wrappedResponse.set(this.cats());
-    //   } catch (e) {
-    //     console.error(e);
-    //     wrappedResponse.set({ error: e });
-    //   }
-
-    //   return wrappedResponse;
-    // },
   });
-
-  // protected readonly rxResource = rxResource({
-  //   request: () => ({}),
-  //   stream: () => this.resource.value(),
-  // });
 }
