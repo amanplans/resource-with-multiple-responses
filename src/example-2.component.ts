b203ec038868d37c2442632ad9cb9ddfb12d2d6e
Andy Man <dev@withaplan.nl>
2025-01-31 14:21:30 +0100

    use update & add defaultValue

--- src/example-2.component.ts
+++ src/example-2.component.ts
@@ -4,27 +4,34 @@ import {
   computed,
   ResourceRef,
   resource,
-} from '@angular/core';
+} from "@angular/core";
 
 @Component({
-  selector: 'app-example-2',
+  selector: "app-example-2",
   template: `
     <h2>Example 2, support multiple responses in resource()</h2>
-    <button type="button" mat-flat-button color="primary" (click)='increaseValue()'>Increase value</button><br/>
+    <button
+      type="button"
+      mat-flat-button
+      color="primary"
+      (click)="increaseValue()"
+    >
+      Increase value</button
+    ><br />
     Value: {{ value() }}
   `,
 })
 export class Example2Component {
-
-  stream = signal<{value: number}>({value: 1});
+  stream = signal<{ value: number }>({ value: 1 });
 
   res: ResourceRef<number | undefined> = resource<number, number>({
-      stream: async () => this.stream,
-    });
+    stream: async () => this.stream,
+  });
 
   value = computed(() => this.res.value() ?? -1);
 
   protected increaseValue() {
-    this.stream.set({value: this.value() + 1});
+    // this.stream.set({ value: this.value() + 1 });
+    this.stream.update((val: { value: number }) => ({ value: val.value + 1 }));
   }
 }
